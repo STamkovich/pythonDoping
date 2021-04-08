6e2cc4d92a22bc5dd77456fb6b88965bd5867d53
Sergey Tamkovich <tamkovich1072@gmail.com>
2021-04-08 17:42:02 +0300

    task_16.py

---
+++ task_16.py
@@ -0,0 +1,13 @@
+# завести список который будет состоять из слов
+# words = ["apple", "banana", "grape", " some other word", "stop", "hello", "goodbye"]
+# цыкл должен остановитсья посл етого как увидит слово "stop" и остальные слова после него
+words = ["apple", "banana", "grape", " some other word", "stop", "hello", "goodbye"]
+i = 0
+while words[i] != "stop":
+    print(words[i])
+    i += 1
+
+for i in range(len(words)):
+    if words[i] == "stop":
+        break
+    print(words[i])
